f11587a4d61db22dfda0d68c3438b29dd56130b8
Amit643.Freelancer <amit643.freelancer@gmail.com>
2022-03-05 23:38:04 +0545

    THIN-3 add Feedback carousel component - update home page with feedback component

---
+++ fixtures/HomePage/feedbacks.ts
@@ -0,0 +1,14 @@
+export const feedbackData = [
+  {
+    feedback: '“Highly recommend -- best plumbers in town! Punctual and professional, Always get the job done well.””',
+    author: 'John Doe',
+  },
+  {
+    feedback: "Honest, reliable, and he's the only plumbers I have ever used.”",
+    author: 'Laura P.',
+  },
+  {
+    feedback: 'When it comes to my plumbing needs, there is no doubt Installers are my "go to" guys',
+    author: 'Martha G',
+  },
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,51 @@
 import type { NextPage } from "next";
 import ImageCarousel from "../components/ImageCarousel";
-import { Box } from "@material-ui/core";
+import { Box, Typography } from "@material-ui/core";
 import { bannerData } from "../fixtures/HomePage/bannerData";
+import FeedbackCarousel from "../components/Feedback";
+import { feedbackData } from "../fixtures/HomePage/feedbacks";
 
 const Home: NextPage = () => {
   return (
     <Box>
       <ImageCarousel height="500px" data={bannerData} />
+      <Box
+        width={1}
+        style={{
+          backgroundColor: "black",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          height: "298px",
+        }}
+        padding="10px"
+      >
+        <Box
+          width={1}
+          style={{
+            maxWidth: "1140px",
+          }}
+        >
+          <Typography
+            component="h2"
+            align="center"
+            style={{
+              color: "white",
+              fontSize: "40px",
+              fontFamily: "'Poppins', Sans-serif",
+              fontWeight: 600,
+              height: 70,
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "flex-end",
+            }}
+          >
+            Feedback
+          </Typography>
+          <FeedbackCarousel height="188px" data={feedbackData} />
+        </Box>
+      </Box>
     </Box>
   );
 };
