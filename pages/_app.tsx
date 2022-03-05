17be6c4395d4b167179197fe38c056a178715a55
Amit643.Freelancer <amit643.freelancer@gmail.com>
2022-03-05 23:33:37 +0545

    THIN-3 parentElement possibly null error bypass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Layout from '../components/layout';
-import { useEffect } from 'react';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/layout";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   //this is done for instance reflection of changes in code into browser
   useEffect(() => {
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
+      jssStyles.parentElement?.removeChild(jssStyles);
     }
   }, []);
   return (
