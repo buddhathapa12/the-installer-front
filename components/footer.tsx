35934c99879dcac37ccc6f3e256a1916034d4015
unknown <buddhat645@gmail.com>
2022-03-08 19:17:22 +0545

    Worked on footer navbar and social handles logo

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,64 @@
+import { AppBar, CssBaseline, Toolbar } from "@material-ui/core";
 import React from "react";
 import useStyles from "../utils/footer.styles";
+import Link from "next/link";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Footer = () => {
   const classes = useStyles();
-  return <div className={classes.footer}> &copy;2022 by the Installer</div>;
+  return (
+    <div className={classes.footerCont}>
+      <AppBar className={classes.navbar}>
+        <CssBaseline />
+        <div className={classes.ContactNumberCont}>Phone Number Here</div>
+        <div className={classes.navbarCont}>
+          <ul className={classes.navbarList}>
+            <li className={classes.navbarItemCont}>
+              <Link href="/">
+                <a className={classes.navbarItem}>Home</a>
+              </Link>
+            </li>
+            <li className={classes.navbarItemCont}>
+              <Link href="/bookOnline">
+                <a className={classes.navbarItem}>Book Online</a>
+              </Link>
+            </li>
+            <li className={classes.navbarItemCont}>
+              <Link href="/about">
+                <a className={classes.navbarItem}>About</a>
+              </Link>
+            </li>
+            <li className={classes.navbarItemCont}>
+              <Link href="/projects">
+                <a className={classes.navbarItem}>Projects</a>
+              </Link>
+            </li>
+            <li className={classes.navbarItemCont}>
+              <Link href="/contact">
+                <a className={classes.navbarItem}>Contact</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className={classes.socialHandleCont}>
+          <span className={classes.socialHandleItemCont}>
+            <FacebookOutlinedIcon />
+          </span>
+          <span className={classes.socialHandleItemCont}>
+            <TwitterIcon />
+          </span>
+          <span className={classes.socialHandleItemCont}>
+            <LinkedInIcon />
+          </span>
+        </div>
+      </AppBar>{" "}
+      <div className={classes.footer}>
+        <p>&copy;2022 by the Installer.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- utils/footer.styles.tsx
+++ utils/footer.styles.tsx
@@ -1,9 +1,80 @@
 import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles({
+  footerCont: {
+    backgroundColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: "1px 0px 0px 0px",
+    borderColor: "#ffffff",
+    padding: "10px 0px 10px 0px",
+  },
+  navbar: {
+    position: "relative",
+    backgroundColor: "inherit",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  ContactNumberCont: {
+    width: "21.726%",
+  },
+  navbarCont: {
+    width: "61.393%",
+    alignContent: "center",
+    alignItems: "center",
+    padding: "10px",
+  },
+  socialHandleCont: {
+    width: "16.097%",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  socialHandleItemCont: {
+    flexGrow: 1,
+    textAlign: "center",
+    borderRadius: "10%",
+    "&:hover": {
+      backgroundColor: "red",
+    },
+  },
+
+  navbarList: {
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    flexWrap: "wrap",
+    padding: 0,
+    margin: 0,
+    listStyleType: "none",
+  },
+
+  navbarItemCont: {
+    flexGrow: 1,
+    textAlign: "center",
+    "&:hover": {
+      "& a": {
+        color: "red",
+        // marginLeft: 10,
+      },
+    },
+  },
+
+  navbarItem: {
+    fontSize: "20px",
+    fontWeight: 500,
+    fontFamily: "Poppins",
+  },
+
   footer: {
     textAlign: "center",
-    fontWeight: 900,
+    fontWeight: 400,
+    fontSize: "15px",
+    color: "#ffffff",
+    marginTop: 0,
+    marginBottom: "0.9rem",
+    fontFamily: "Poppins",
   },
 });
 
