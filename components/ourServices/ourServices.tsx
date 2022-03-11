7db26a6cfce808bb58ac12e1b078eee77a210c28
unknown <buddhat645@gmail.com>
2022-03-11 22:24:58 +0545

    Adding the styles of our services in mobile view

--- components/ourServices/ourServices.tsx
+++ components/ourServices/ourServices.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from "@mui/material";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LightbulbOutlinedIcon from "@mui/icons-material/LightbulbOutlined";
 import EngineeringOutlinedIcon from "@mui/icons-material/EngineeringOutlined";
 import HotelOutlinedIcon from "@mui/icons-material/HotelOutlined";
@@ -16,25 +16,40 @@ import useStyles from "./ourServices.styles";
 
 const OurServices = () => {
   const classes = useStyles();
+
+  const [mobileView, setmobileView] = useState(false);
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth <= 766 ? setmobileView(true) : setmobileView(false);
+    };
+
+    setResponsiveness();
+    window.addEventListener("resize", () => setResponsiveness());
+
+    return () => {
+      window.removeEventListener("resize", () => setResponsiveness());
+    };
+  }, []);
+
   return (
     <Box className={classes.ourServiceCont}>
       <Typography className={classes.ourServicesTitle}>Our Services</Typography>
-      <div className={classes.ourServiceSubCont}>
-        <Box className={classes.ourServiceItemCont}>
+      <div className={mobileView ? classes.ourServiceMobileSubCont : classes.ourServiceSubCont}>
+        <Box className={mobileView ? classes.ourServiceMobileItemCont : classes.ourServiceItemCont}>
           <div className={classes.serviceIcon}>
             <LightbulbOutlinedIcon />
           </div>
           <Typography className={classes.serviceName}>Changing door knobs or locks.</Typography>
           <Typography className={classes.serviceDescription}>changing door knobs or locks from $80.</Typography>
         </Box>
-        <Box className={classes.ourServiceItemCont}>
+        <Box className={mobileView ? classes.ourServiceMobileItemCont : classes.ourServiceItemCont}>
           <div className={classes.serviceIcon}>
             <EngineeringOutlinedIcon />
           </div>
           <Typography className={classes.serviceName}>Vanity Install.</Typography>
           <Typography className={classes.serviceDescription}>Vanity Installs from $180.</Typography>
         </Box>
-        <Box className={classes.ourServiceItemCont}>
+        <Box className={mobileView ? classes.ourServiceMobileItemCont : classes.ourServiceItemCont}>
           <div className={classes.serviceIcon}>
             <HotelOutlinedIcon />
           </div>
@@ -42,22 +57,22 @@ const OurServices = () => {
           <Typography className={classes.serviceDescription}>Furniture Assembly from $100.</Typography>
         </Box>
       </div>
-      <div className={classes.ourServiceSubCont}>
-        <Box className={classes.ourServiceItemCont}>
+      <div className={mobileView ? classes.ourServiceMobileSubCont : classes.ourServiceSubCont}>
+        <Box className={mobileView ? classes.ourServiceMobileItemCont : classes.ourServiceItemCont}>
           <div className={classes.serviceIcon}>
             <LocalLaundryServiceIcon />
           </div>
           <Typography className={classes.serviceName}>Washer and Dryer Installs.</Typography>
           <Typography className={classes.serviceDescription}>Washer and Dryer Installs from $60.</Typography>
         </Box>
-        <Box className={classes.ourServiceItemCont}>
+        <Box className={mobileView ? classes.ourServiceMobileItemCont : classes.ourServiceItemCont}>
           <div className={classes.serviceIcon}>
             <DiningOutlinedIcon />
           </div>
           <Typography className={classes.serviceName}>Sink Install.</Typography>
           <Typography className={classes.serviceDescription}>Sink Installs with faucet from $150.</Typography>
         </Box>
-        <Box className={classes.ourServiceItemCont}>
+        <Box className={mobileView ? classes.ourServiceMobileItemCont : classes.ourServiceItemCont}>
           <div className={classes.serviceIcon}>
             <TvOutlinedIcon />
           </div>
@@ -65,22 +80,22 @@ const OurServices = () => {
           <Typography className={classes.serviceDescription}>TV Wall Mounting from $120.</Typography>
         </Box>
       </div>
-      <div className={classes.ourServiceSubCont}>
-        <Box>
+      <div className={mobileView ? classes.ourServiceMobileSubCont : classes.ourServiceSubCont}>
+        <Box className={mobileView ? classes.ourServiceMobileItemCont : classes.ourServiceItemCont}>
           <div className={classes.serviceIcon}>
             <DirectionsRunOutlinedIcon />
           </div>
           <Typography className={classes.serviceName}>Exercise Equipment Assembly.</Typography>
           <Typography className={classes.serviceDescription}>Exercise Equipment Assembly from $100.</Typography>
         </Box>
-        <Box>
+        <Box className={mobileView ? classes.ourServiceMobileItemCont : classes.ourServiceItemCont}>
           <div className={classes.serviceIcon}>
             <KitchenOutlinedIcon />
           </div>
           <Typography className={classes.serviceName}>Fridge Waterline Install.</Typography>
           <Typography className={classes.serviceDescription}>Fridge Waterline Installs from $100.</Typography>
         </Box>
-        <Box>
+        <Box className={mobileView ? classes.ourServiceMobileItemCont : classes.ourServiceItemCont}>
           <div className={classes.serviceIcon}>
             <BlenderOutlinedIcon />
           </div>
