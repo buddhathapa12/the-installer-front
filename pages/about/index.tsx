78b494828c4e1563bf7bed783bfbbabbe09b7eaf
unknown <buddhat645@gmail.com>
2022-03-16 00:18:46 +0545

    Changing the height of the ImageBanner section in about us page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -17,6 +17,7 @@ const About = () => {
           backgroundImageUrl: "/static/images/about-us/plumbing-hero-pipes.jpg",
         }}
         titleFontSize="42px"
+        height="400px"
       />
       <Box className={`${classes.page} ${classes.backgroundImage}`}>
         <Container className={classes.aboutUsCont}>
